42bf4d0dc8e0d7116622df7c31944758d7865a21
jessechen04 <92489058+jessechen04@users.noreply.github.com>
2025-06-07 20:58:51 -0400

    Completed

--- LoyaltyFunctionApp/src/functions/CreatePass.ts
+++ LoyaltyFunctionApp/src/functions/CreatePass.ts
@@ -14,28 +14,30 @@ const key = fs.readFileSync(path.join(__dirname, '..', '..', '..', 'certs', 'key
 const wwdr = fs.readFileSync(path.join(__dirname, '..', '..', '..', 'certs', 'AppleWWDR.pem'));
 
 export async function CreatePass(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
-    context.log(`Http functions processed request for url "${request.url}"`);
+    //context.log(`Http functions processed request for url "${request.url}"`);
 
     try {
         // "as CreatePassRequestBody" to make sure it uses the interface
         // Basically this is a type assertion to ensure TypeScript knows the shape of the request body
         const body = await request.json() as CreatePassRequestBody; // <-- THIS is your "req.body"
-        context.log("Parsed request body:", body);
+        //context.log("Parsed request body:", body);
         const userCode = body.userCode ?? "123"; // Default to "123" if not provided
         // const userId = request.query.get('userId') ?? "123";
 
-        const pass = await PKPass.from({
-            model: path.join(__dirname, '..', '..', '..', 'loyalty_pass'),
-            certificates: {
-            wwdr: wwdr,
-            signerCert: cert,
-            signerKey: key,
+        const pass = await PKPass.from(
+            {
+                model: path.join(__dirname, '..', '..', '..', 'loyalty_pass'),
+                certificates: {
+                    wwdr: wwdr,
+                    signerCert: cert,
+                    signerKey: key,
+                }
+            },
+            {
+                serialNumber: `user-${userCode}`,
             }
-        });
+    );
 
-        context.log("Pass object created");
-
-        pass.serialNumber = `user-${userCode}-v3`;
         const barcode = {
             message: `${userCode}`,
             format: 'PKBarcodeFormatQR',
@@ -43,11 +45,16 @@ export async function CreatePass(request: HttpRequest, context: InvocationContex
             altText: `Loyalty #: ${userCode}`,
         };
 
-        pass.barcode = barcode;   // Backward compatibility
-        pass.barcodes = [barcode]; // Modern iOS uses this
-        pass.description = `Wallet pass for user ${userCode}`;
+        pass.setBarcodes(barcode); // Modern iOS uses this
 
-        //context.log("Pass object created 2");
+    pass.headerFields.push({
+        "key": "loyalty",
+        "label": "Loyalty ID",
+        "value": `${userCode}`,
+        "textAlignment": "PKTextAlignmentRight",
+	});
+
+        //context.log("Pass object created");
 
         // context.log('Generating pass for:', userId);
         // context.log('Using model:', path.join(__dirname, 'loyalty_pass'));
